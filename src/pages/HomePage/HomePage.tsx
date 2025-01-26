ddb0507fb8e2d5e243bd569bcc6eee18e0c7b05d
justJubair <jubair.ahmed2838@gmail.com>
2025-01-27 00:02:08 +0600

    [Complete]: banner

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@/components/ui/button";
 import bannerImg from "../../assets/images/banner.jpg";
 const HomePage = () => {
   return (
-    <div className="relative">
-      <img src={bannerImg} alt="" />
+    <div className="relative w-screen h-screen">
+      <div className="absolute inset-0 bg-black">
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#ffffff20_1px,transparent_1px),linear-gradient(to_bottom,#ffffff20_1px,transparent_1px)] bg-[size:52px_52px]">
+          <div className="absolute inset-0 animate-grid-shift bg-[radial-gradient(#ffffff50_1px,transparent_1px)] bg-[size:52px_52px] opacity-50"></div>
+        </div>
+      </div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center z-30">
+        <h1 className=" monage-font text-6xl text-white">
+          Quiz. Compete. Conquer.
+        </h1>
+        <p className="text-center text-xl text-[#ffffffa2]">
+          Multiplayer quizzes that keep you on your toes. Play now and show off
+          your smarts
+        </p>
+        <div className="flex items-center gap-4 mt-10">
+          <Button className="rounded-xl bg-[#ffffffe0] text-[#000000dd] hover:text-[#000] hover:bg-white">
+            Join now
+          </Button>
+          <Button className="rounded-xl text-[#ffffffa2] hover:text-[#fff]">
+            Create a lobby
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      keyframes: {
+        "grid-shift": {
+          "0%": { transform: "translateX(0) translateY(0)" },
+          "100%": { transform: "translateX(-52px) translateY(-52px)" },
+        },
+      },
+      animation: {
+        "grid-shift": "grid-shift 5s linear infinite",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
