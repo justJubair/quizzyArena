b4a9df3b4e3ed75b977c50f98b545d7b499b4093
justJubair <jubair.ahmed2838@gmail.com>
2025-01-27 23:42:52 +0600

    [Add]: banner text reponsive

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,14 +9,14 @@ const HomePage = () => {
         </div>
       </div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center z-30">
-        <h1 className=" monage-font text-6xl text-white">
+        <h1 className=" monage-font text-white text-6xl text-center w-[1000px]">
           Quiz. Compete. Conquer.
         </h1>
-        <p className="text-center text-xl text-[#ffffffa2]">
+        <p className="text-center text-[#ffffffa2] text-lg">
           Multiplayer quizzes that keep you on your toes. Play now and show off
           your smarts
         </p>
-        <div className="flex items-center gap-4 mt-10">
+        <div className="flex items-center gap-4 mt-7">
           <Link to="/game">
             <Button className="rounded-xl bg-[#ffffffe0] text-[#000000dd] hover:text-[#000] hover:bg-white">
               Play now
